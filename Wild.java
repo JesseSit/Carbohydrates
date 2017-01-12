eb470d21f4e0f8352145b38332f7ebbe85f9f796
JoshdelX <sxiedeng@stuy.edu>
2017-01-12 07:43:13 -0500

    Wild v1.2

--- Wild.java
+++ Wild.java
@@ -1,5 +1,13 @@
 public class Wild extends Property {
     private String color0;
     private String color1;
-    private boolean Multi_color;  
+    private boolean Multi_color; 
+
+    public Wild (){
+	super();
+	color0 = "";
+	color1 = "";
+	Multi_color = true;
+    }
+
 }
