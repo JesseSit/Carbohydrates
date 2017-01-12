f78b74d75db71ef4130935f51abcb832c8234283
JoshdelX <sxiedeng@stuy.edu>
2017-01-12 07:29:57 -0500

    Developer Log

--- Wild.java
+++ Wild.java
@@ -1,4 +1,6 @@
 public class Wild extends Property {
-
-
+    private String color0;
+    private String color1;
+    private boolean Multi_color;
+    
 }
--- woo.java
+++ woo.java
@@ -1,6 +1,11 @@
 import cs1.Keyboard;
 
+//game executer file
 public class woo{
+    
+    
+
     public static void main(String[] args) {
+
     }
 }
