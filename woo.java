b01f4c61910a0f9147c4676b3a50d27f8411b583
Xing Tao Shi <xshi@stuy.edu>
2017-01-12 08:35:34 -0500

    woo edits

--- woo.java
+++ woo.java
@@ -2,6 +2,26 @@ import cs1.Keyboard;
 
 public class woo{
     public static void main(String[] args) {
-	int rows = Keyboard.readInt();
+	//setup game by shuffling deck
+	System.out.println("Please select the number of players (2-4)");
+	int amtPlayers = Keyboard.readInt();
+	if (amtPlayers == 2) {
+	    Player human0 = new Human(); 
+	    Player human1 = new Human(); 
+	}
+	else if (amtPlayers == 3) {
+	    Player human0 = new Human(); 
+	    Player human1 = new Human(); 
+	    Player human2 = new Human(); 
+	}
+	else if (amtPlayers == 4) {
+	    Player human0 = new Human(); 
+	    Player human1 = new Human(); 
+	    Player human2 = new Human();
+ 	    Player human3 = new Human(); 
+	}
+	else {
+	    System.out.println("Not in player range!");
+	}
     }
 }
