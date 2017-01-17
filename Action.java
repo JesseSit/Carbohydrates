2570acb3325b5a3768bdd8726ebf613057b7d254
JoshdelX <sxiedeng@stuy.edu>
2017-01-17 07:23:46 -0500

    Action v1.2

--- Action.java
+++ Action.java
@@ -1,4 +1,20 @@
 public class Action extends Cards {
     private String name;
     private String desc;
+
+    public Action() {
+	super();
+	name = "";
+	desc = "";
+    }
+
+    public Action(int Val, int id, String newName, String newDesc) {
+	super(Val, id);
+	name = newName;
+	desc = newDesc;
+    }
+
+    public String readDesc() {
+	return desc;
+    }
 }
