964f4eefc5739f577c7022c4bd23feee0d9c6f08
JoshdelX <sxiedeng@stuy.edu>
2017-01-17 07:44:26 -0500

    Player v1.2

--- Player.java
+++ Player.java
@@ -2,9 +2,13 @@ import java.util.ArrayList;
 
 public class Player {
     protected ArrayList hand;
+    protected ArrayList bank;
+    protected ArrayList properties;
+   
     public void play(int index){
 	System.out.println(hand.remove(index));
     }
+
     public void draw(int amtDraw, ArrayList<Cards> deck) {
 	for (int i = 0; i < amtDraw; i++) {
 	    hand.add(deck.remove(0));
@@ -17,4 +21,9 @@ public class Player {
 	hand.add(temp);
     }
 
+    public void addProperty(int index) {
+	if ((Cards)hand.get(index).getID() < 10) {
+	    bank.add(hand.remove(index));
+	}
+    }
 }
