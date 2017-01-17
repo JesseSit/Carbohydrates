19df37587e57cd76fc0c22c286abac35e96b4c95
Xing Tao Shi <xshi@stuy.edu>
2017-01-17 08:23:21 -0500

    Rent changes

--- Rent.java
+++ Rent.java
@@ -1,6 +1,21 @@
 public class Rent extends Cards {
-    public String color0;
-    public String color1;
-    public boolean Multicolor;
+    private String color0;
+    private String color1;
+    private boolean Multicolor;
+    private boolean payAll;
 
+    public Rent (){
+	super;
+	color0 = "NA";
+	color1 = "NA";
+	Multicolor = false;
+	payAll = false
+    }
+
+    public Rent(String Color0, String Color1, boolean multi, boolean payall) {
+	color0 = Color0;
+	color1 = Color1;
+	Multicolor = multi;
+	payAll = payall
+    }
 }
