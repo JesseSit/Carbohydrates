99fa0590bf55fad8b936e64815311406d4414c2b
JoshdelX <sxiedeng@stuy.edu>
2017-01-18 22:07:55 -0500

    End Turn added

--- Woo.java
+++ Woo.java
@@ -7,9 +7,8 @@ public class Woo{
     public static void main(String[] args) {
 	int turns = 0;
 	int cardNum = 0;
-	//setup game by shuffling deck
 	
-	//create deck
+//CREATE DECK ==================================================================
 	ArrayList <Cards> deck = new ArrayList<Cards>();
 	Cards temp = new DarkBlue("boardwalk", 4, 0, 3, 8, 0, 0, 2);
 	Cards temp1 = new DarkBlue("park place", 4, 0, 3, 8, 0, 0, 2);
@@ -24,7 +23,8 @@ public class Woo{
 	}
 	
 	System.out.println("Please select the number of players (2-4):");
-	//create players
+
+//CREATE PLAYERS ===============================================================
 	int amtPlayers = Keyboard.readInt();
 	if (amtPlayers > 4 || amtPlayers < 2) {
 	    System.out.println("Not in player range!");
@@ -58,51 +58,80 @@ public class Woo{
 	    h3 = new Human(name3);
 	    h3.draw(5, deck);
 	}
-	
+
+//PLAY GAME ====================================================================
 	int index;
 	while (cardNum < 3){
+	    //PLAYER 1 =========================================================
 	    if ((turns % amtPlayers) == 0){
 		if (cardNum == 0){
 		    h0.draw(2, deck);
 		}
 		System.out.println(h0);
 		System.out.println("Pick the index of the card you want to play:");
+		System.out.println("Or: Enter 99 if you would like to end your turn.");
 		index = Keyboard.readInt();
-		if (((Cards)(h0.hand.get(index))).getID() < 11){
-		    h0.addProperty(index);
+		if (index == 99) {
+		    turns += 1;
+		}
+		else {
+		    if (((Cards)(h0.hand.get(index))).getID() < 11){
+			h0.addProperty(index);
+		    }
 		}
 	    }
+	    //PLAYER 2 =========================================================
 	    if ((turns % amtPlayers) == 1){
 		if (cardNum == 0){
 		    h1.draw(2, deck);
 		}
 		System.out.println(h1); 
 		System.out.println("Pick the index of the card you want to play:");
+		System.out.println("Or: Enter 99 if you would like to end your turn.");
 		index = Keyboard.readInt();
-		if (((Cards)(h1.hand.get(index))).getID() < 11){
-		    h1.addProperty(index);
+		if (index == 99) {
+		    turns += 1;
+		}
+		else {
+		    if (((Cards)(h1.hand.get(index))).getID() < 11){
+			h1.addProperty(index);
+		    }
 		}
 	    }
+	    //PLAYER 3 =========================================================
 	    if ((turns % amtPlayers) == 2){
 		if (cardNum == 0){
 		    h2.draw(2, deck);
 		}
 		System.out.println(h2);
 		System.out.println("Pick the index of the card you want to play:");
+		System.out.println("Or: Enter 99 if you would like to end your turn.");
 		index = Keyboard.readInt();
-		if (((Cards)(h2.hand.get(index))).getID() < 11){
-		    h2.addProperty(index);
+		if (index == 99) {
+		    turns += 1;
+		}
+		else {
+		    if (((Cards)(h2.hand.get(index))).getID() < 11){
+			h2.addProperty(index);
+		    }
 		}
 	    }
+	    //PLAYER 4 =========================================================
 	    if ((turns % amtPlayers) == 3){
 		if (cardNum == 0){
 		    h3.draw(2, deck);
 		}
 		System.out.println(h3);
 		System.out.println("Pick the index of the card you want to play:");
+		System.out.println("Or: Enter 99 if you would like to end your turn.");
 		index = Keyboard.readInt();
-		if (((Cards)(h3.hand.get(index))).getID() < 11){
-		    h3.addProperty(index);
+		if (index == 99) {
+		    turns += 1;
+		}
+		else {
+		    if (((Cards)(h3.hand.get(index))).getID() < 11){
+			h3.addProperty(index);
+		    }
 		}
 	    }
 	    cardNum += 1;
@@ -110,16 +139,6 @@ public class Woo{
 		cardNum = 0;
 		turns += 1;
 	    }
-	}
-	
-	
-
-	//deal hands
-	
-	
-	
-	
-	
+	}		
     }
-
 }
