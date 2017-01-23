038f28d0fe84bb9ab08f61dae51fc37143ff9fe2
JoshdelX <sxiedeng@stuy.edu>
2017-01-22 19:17:37 -0500

    adding Wild to deck

--- Wild.java
+++ Wild.java
@@ -10,5 +10,10 @@ public class Wild extends Property {
 	Multi_color = true;
     }
     
-
+    public Wild(int Val, int id, int rentVal0, int rentVal1, int rentVal2, int rentVal3, int setNum, String c0, String c1, boolean x) {
+	super(Val, id, rentVal0, rentVal1, rentVal2, rentVal 3, setNum, c0, c1, x);
+	color0 = c0;
+	color1 = c1;
+	Multi_color = x;
+    }
 }
